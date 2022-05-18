ef3987777fd2af7a8642e19fe3ac8071e5983e12
rakhi2207 <jharakhi1211@gmail.com>
2022-05-18 09:18:01 +0530

    VarHoisting

--- varExample.js
+++ varExample.js
@@ -1,9 +1,39 @@
 //var is function scoped but not block scoped
-var varName=10;
-function fn()
-{
-    var varName=20;
-    console.log(varName)
+// var varName=10;
+// function fn()
+// {
+//     console.log(varName)
+//     var varName=20;
+//     console.log(varName)
+// }
+// fn();
+// console.log(varName)
+
+// var varName=10;
+// //function fn()
+// {
+
+//    console.log("fn check "+varName);
+//    varName=90;
+// }
+// //fn();
+// console.log(varName)
+
+
+const fullName = "Oluwatobi Sofela";
+
+// Nested functions containing two more fullName variables:
+function profile() {
+  const fullName = "Tobi Sho";
+  function sayName() {
+    const fullName = "Oluwa Sofe";
+    function writeName() {
+      return fullName;
+    }
+    return writeName();
+  }
+  return sayName();
 }
-fn();
-console.log(varName)
+
+console.log(profile());
+
