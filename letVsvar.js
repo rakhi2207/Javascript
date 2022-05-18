e5d268e24f544cdbaf96851a04116d056e9232a1
rakhi2207 <jharakhi1211@gmail.com>
2022-05-18 11:15:53 +0530

    Scope

---
+++ letVsvar.js
@@ -0,0 +1,9 @@
+let letFruit="orange";
+var varFruit="orange";
+console.log(letFruit+" "+varFruit);
+{
+    letFruit="apple";
+    varFruit="apple";
+    console.log(letFruit+" "+varFruit);
+}
+console.log(letFruit+" "+varFruit);
